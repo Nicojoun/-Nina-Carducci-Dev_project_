462b5561ff1822cbba636ea64e1fc5f8c5b8bfe1
Nico Joun <nicojoun793@gmail.com>
2024-07-30 10:32:12 +0200

    les boutons pour passer d'une image à l'autre dans la div gallery fonctionnent

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -150,7 +150,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
+          index = i - 1 ;
         }
       });
       next =
@@ -189,7 +189,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
+          index = i + 1;
         }
       });
       next = imagesCollection[index] || imagesCollection[0];
@@ -262,13 +262,8 @@
                   $(this).parents(".item-column").hide();
               }
           });
-      }
-  }
-  
-  
-  
+        }
+    }
 
-  
-  
   };
 })(jQuery);
